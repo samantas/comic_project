0df79470bf30e77eeb7de4c50297df79d0fb6f20
Samanta Shi <samantashi@Samantas-MacBook-Pro-2.local>
2019-03-14 17:40:14 +0800

    added img and to user flow

--- js/script.js
+++ js/script.js
@@ -16,17 +16,14 @@ let coffee = document.getElementById("coffee");
 let snooze = document.getElementById("snooze");
 let wakeup = document.getElementById("wakeup");
 
-// hide character images here so that we only display the relevant ones
-cafe.style.display = "none";
-coffee.style.display = "none";
-
 // first scene lead into decision
 setTimeout(function() {
     decision_overlay.style.display = "flex";
     iphone_character.style.display = "none";
-}, 3000);
+}, 1000);
 
 
+// -------------------------------------
 // begin event listener code for decisions
 decision_one.addEventListener("mouseenter", mouseEnter);
 decision_two.addEventListener("mouseenter", mouseEnter);
@@ -49,37 +46,77 @@ function mouseLeave(e) {
     }
 }
 // end event listener code for decisions
+// -------------------------------------
 
+
+// -------------------------------------
 // begin event listener code for character CTAs
-snooze.addEventListener("click", clicked);
-wakeup.addEventListener("click", clicked);
+let img_one = document.getElementById("img_one");
+let img_two = document.getElementById("img_two");
+img_one.addEventListener("click", clicked);
+img_two.addEventListener("click", clicked);
 
 function clicked(e) {
-    if (this == snooze) {
+
+    if (this.classList.contains("snooze")) {
         
-        // change to the path that happens after snooze
+        this.classList.remove("snooze");
         background.src = "img/bg_subway.png";
         title.innerHTML = "Oh no! You missed the shuttle and have to take the subway."
 
         decision_overlay.style.display = "none";
-        snooze.style.display = "none";
 
         setTimeout(function() {
-            background.src = "img/bg_entrance.png"
-            title.innerHTML = "...but you finally arrive at school. You are hungry. Where should you go for breakfast?";
+            background.src = "img/bg_entrance.png";
+            title.innerHTML = "...but you finally arrive at school. You are late. Eat or go to class?";
             decision_overlay.style.display = "flex";
-            cafe.style.display = "block";
-            description_one.innerHTML = "Cafe";
-            coffee.style.display = "block";
+
+            img_one.src = "img/study.png";
+            img_one.classList.add("study");
+            description_one.innerHTML = "Class";
+
+            img_two.src = "img/coffee.png";
+            img_two.classList.add("starbucks");
+            img_two.classList.remove("wakeup");
             description_two.innerHTML = "Starbucks";
-        }, 3000);
 
-    } else if (this == wakeup) {
-        
+        }, 1000);
+
+    } else if (this.classList.contains("wakeup")) {
+
         // change to the path that happens after waking up
-
         // don't have shuttle pic yet?
         // background.src = "img/bg_shuttle.png"
 
+    } else if (this.classList.contains("cafe")) {
+
+    } else if (this.classList.contains("starbucks")) {
+    	decision_overlay.style.display = "none";
+    	this.classList.remove("starbucks");
+    	background.src = "img/bg_SB.png";
+    	title.innerHTML = "You wait for your coffee and head back to the AB.";
+    	
+    	setTimeout(function(){
+    		decision_overlay.style.display = "flex";
+    		background.src = "img/bg_entrance.png";
+
+    		img_one.src = "img/study.png";
+            img_one.classList.add("study");
+            description_one.innerHTML = "Go to class late.";
+
+            img_two.src = "img/coffee.png";
+            img_two.classList.add("lounge");
+            img_two.classList.remove("starbucks");
+            description_two.innerHTML = "Do homework at the lounge";
+
+    	}, 3000);
+
+    } else if (this.classList.contains("class")) {
+    	background.src = "img/bg_class.HEIC";
+    	title.innerHTML = "Class";
     }
 }
+
+
+
+
