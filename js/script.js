ed165676a79d16fa164cd0a6f4d3c4d8d1edd3f8
Samanta Shi <samantashi@Samantas-MacBook-Pro-2.local>
2019-03-14 15:05:31 +0800

    added interaction on snooze

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-setTimeout(function(){
-	let decision_overlay = document.getElementById("container_overlay");
-	let iphone_character = document.getElementById("iphone");
-	decision_overlay.style.display = "flex";
-	iphone_character.style.display = "none";
+setTimeout(function() {
+    let decision_overlay = document.getElementById("container_overlay");
+    let iphone_character = document.getElementById("iphone");
+    decision_overlay.style.display = "flex";
+    iphone_character.style.display = "none";
 }, 3000);
 
 let decision_one = document.getElementById("decision_one");
@@ -35,16 +35,27 @@ function mouseLeave(e) {
 
 let snooze = document.getElementById("snooze");
 let wakeup = document.getElementById("wakeup");
+let background = document.getElementById("background");
+let title = document.getElementById("title");
 
 snooze.addEventListener("click", clicked);
 wakeup.addEventListener("click", clicked);
 
 function clicked(e) {
-	if (this == snooze) {
-		console.log("snooze");
-		// change to the path that happens after snooze
-	} else if (this == wakeup) {
-		console.log("wakeup");
-		// change to the path that happens after waking up
-	}
+    if (this == snooze) {
+        console.log("snooze");
+        // change to the path that happens after snooze
+        background.src = "img/bg_subway.png";
+        title.innerHTML = "Oh no! You missed the shuttle and have to take the subway."
+
+        // need to change the character cta here too
+        // probably need to setTimeout to have transition between
+        // the scene and decisions
+
+    } else if (this == wakeup) {
+        console.log("wakeup");
+        // change to the path that happens after waking up
+        // don't have shuttle pic yet
+        // background.src = "img/bg_shuttle.png"
+    }
 }
