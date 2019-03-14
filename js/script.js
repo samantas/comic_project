8a9c0a7695decc7f5408e52cc7b96dd2bdd700c8
Samanta Shi <samantashi@Samantas-MacBook-Pro-2.local>
2019-03-14 16:17:03 +0800

    expanded the snooze path

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+let decision_overlay = document.getElementById("container_overlay");
+let iphone_character = document.getElementById("iphone");
+
+let description_one = document.getElementById("description_one");
+let description_two = document.getElementById("description_two");
+
+let cafe = document.getElementById("cafe");
+let coffee = document.getElementById("coffee");
+cafe.style.display = "none";
+coffee.style.display = "none";
+
 setTimeout(function() {
-    let decision_overlay = document.getElementById("container_overlay");
-    let iphone_character = document.getElementById("iphone");
     decision_overlay.style.display = "flex";
     iphone_character.style.display = "none";
 }, 3000);
@@ -14,23 +23,19 @@ decision_one.addEventListener("mouseleave", mouseLeave);
 decision_two.addEventListener("mouseleave", mouseLeave);
 
 function mouseEnter(e) {
-
     if (this == decision_one) {
         decision_two_overlay.style.display = "block";
     } else if (this == decision_two) {
         decision_one_overlay.style.display = "block";
     }
-
 }
 
 function mouseLeave(e) {
-
     if (this == decision_one) {
         decision_two_overlay.style.display = "none";
     } else if (this == decision_two) {
         decision_one_overlay.style.display = "none";
     }
-
 }
 
 let snooze = document.getElementById("snooze");
@@ -43,17 +48,26 @@ wakeup.addEventListener("click", clicked);
 
 function clicked(e) {
     if (this == snooze) {
-        console.log("snooze");
+        
         // change to the path that happens after snooze
         background.src = "img/bg_subway.png";
         title.innerHTML = "Oh no! You missed the shuttle and have to take the subway."
 
-        // need to change the character cta here too
-        // probably need to setTimeout to have transition between
-        // the scene and decisions
+        decision_overlay.style.display = "none";
+        snooze.style.display = "none";
+
+        setTimeout(function() {
+            background.src = "img/bg_entrance.png"
+            title.innerHTML = "...but you finally arrive at school. You are hungry. Where should you go for breakfast?";
+            decision_overlay.style.display = "flex";
+            cafe.style.display = "block";
+            description_one.innerHTML = "Cafe";
+            coffee.style.display = "block";
+            description_two.innerHTML = "Starbucks";
+        }, 3000);
 
     } else if (this == wakeup) {
-        console.log("wakeup");
+        
         // change to the path that happens after waking up
         // don't have shuttle pic yet
         // background.src = "img/bg_shuttle.png"
